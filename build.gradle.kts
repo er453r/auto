2421a78de70969dc08a6279369a0088a986428c8
Marcin Kotz <marcin@kotz.pl>
2024-08-26 17:13:10 +0200

    feat: added openapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.4")
     implementation("org.zeroturnaround:zt-exec:1.12")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
