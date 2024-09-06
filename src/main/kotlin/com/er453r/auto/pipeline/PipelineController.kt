55c5f809856853fe6662362c3da6915792580719
Marcin Kotz <marcin@kotz.pl>
2024-09-06 21:02:15 +0200

    feat: fetch single pipeline

--- src/main/kotlin/com/er453r/auto/pipeline/PipelineController.kt
+++ src/main/kotlin/com/er453r/auto/pipeline/PipelineController.kt
@@ -1,8 +1,10 @@
 package com.er453r.auto.pipeline
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("pipelines")
@@ -11,4 +13,7 @@ class PipelineController(
 ) {
     @GetMapping
     fun list(): List<Pipeline> = pipelineRepository.findAll()
+
+    @GetMapping("{id}")
+    fun get(@PathVariable id: UUID): Pipeline = pipelineRepository.getReferenceById(id)
 }
